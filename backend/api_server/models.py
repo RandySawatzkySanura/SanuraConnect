e087eb8109aaf2d446482475d2a4d975261c4e45
RandySawatzkySanura <Randy@sanurapetcare.com>
2024-07-15 09:34:50 -0600

    add models and serializers;

--- backend/api_server/models.py
+++ backend/api_server/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class InventoryWebSupplement(models.Model):
+    sku = models.CharField(primary_key=True, max_length=20)
+    brand = models.CharField(max_length=100, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
+    department_web = models.CharField(max_length=100, blank=True, null=True)
+    description_short = models.CharField(max_length=100, blank=True, null=True)
+    keyFeature1 = models.CharField(max_length=1000, blank=True, null=True)
+    keyFeature2 = models.CharField(max_length=1000, blank=True, null=True)
+    keyFeature3 = models.CharField(max_length=1000, blank=True, null=True)
+    keyFeature4 = models.CharField(max_length=1000, blank=True, null=True)
+    description_web = models.CharField(max_length=3000, blank=True, null=True)
+    pack_length = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    pack_width = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    pack_height = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    pack_weight = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    burried_freight = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    price_web = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    pack_type = models.CharField(max_length=10, blank=True, null=True)
+    web_enabled = models.BooleanField()
+    dim_weight_lb_ups = models.DecimalField (max_digits=5,decimal_places=2, blank=True, null=True)
+    dim_weight_lb_canadapost = models.DecimalField (max_digits=5,decimal_places=2, blank=True, null=True)
+    shipping_class = models.CharField(max_length=15, blank=True, null=True)
+    web_category_1 = models.IntegerField()
+    web_category_2 = models.IntegerField()
+    web_category_3 = models.IntegerField()
+
+
+
+    class Meta:
+        managed = False
+        db_table = 'inventory_web_supplement'
+        db_table_comment = 'a table to store web information'
